cfa32a01173ce8d3ffcd37eabae7d502832ea4e5
RjRishuSty <rishurajk59@gmail.com>
2025-07-13 10:16:39 +0530

    add MobileFilter.jsx

--- frontend/src/pages/CategoryPage.jsx
+++ frontend/src/pages/CategoryPage.jsx
@@ -8,8 +8,8 @@ import { handleUserData } from "../redux/slices/userData.slice";
 import { useDispatch, useSelector } from "react-redux";
 import SearchInput from "../components/SearchInput";
 import SkeletonLoader from "../components/SkeletonLoader";
-import MobileFilter from "../components/Mobilefilter";
 import { handleGetHeading } from "../redux/slices/filterData.slice";
+import MobileFilter from "../components/Mobilefilter";
 
 const CategoryPage = () => {
   //* All states...............
